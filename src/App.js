14053d88713ed30fc8223f44e6a8fc45600be3ce
Thuy Nguyen <thuynm@visionnetwork.vn>
2023-01-30 14:39:54 +0700

    Completed post app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Counter from "./features/counter/Counter";
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <AddPostForm />
+      <PostsList />
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import postReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/userSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postReducer,
+    users: usersReducer,
   },
 });
